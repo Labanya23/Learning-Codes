111d03c6d07d75602ce1555dadfd9f7471c49d99
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-10-15 23:03:34 +0600

    Update Scarborough Fair.cpp

--- Scarborough Fair.cpp
+++ Scarborough Fair.cpp
@@ -7,15 +7,15 @@ int main(){
     int n,m;
     cin>>n>>m;
     scanf("%d %d %*c",&n,&m);
-    char str[n+1];
+    char str[n+1];//FOR NULL CHARACTER
     scanf("%s",str);
     int l,r;
     char c1,c2;
     while(m--){
         scanf("%d %d %c %c",&l,&r,&c1,&c2);
         for(int i=l-2;i<r;i++){
-            if(str[i]==c1){
-                str[i]=c2;
+            if(str[i]==c1){ //I=I
+                str[i]=c2;//noi
             }
         }
     }
