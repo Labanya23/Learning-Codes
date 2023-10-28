934dc3e96dfb5421a633cb1726042c44eb147e29
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-10-28 17:34:32 +0600

    Add files via upload

---
+++ In search of an easy problem.cpp
@@ -0,0 +1,39 @@
+//CODEFORCE PRAC 34
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+   int t;
+   cin>>t;
+   int flag=0;
+   while(t--){
+    //int flag=0;
+    int k;
+    cin>>k;
+    int count1=0;
+    if(k==1)
+    {
+        count1++;
+        flag=1;
+        break;
+    }
+   }
+   if(flag==1)
+   {
+       cout<<"HARD"<<endl;
+   }
+   else{
+    cout<<"EASY"<<endl;
+   }
+     return 0;
+  }
+  /*
+  3
+0 0 1
+
+output--HARD
+1
+0
+output-easy
+*/
