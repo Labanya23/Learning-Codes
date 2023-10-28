32b9831c2d66f247217233ae0be80565f74b65ed
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-10-28 11:12:27 +0600

    Add files via upload

---
+++ Translation.cpp
@@ -0,0 +1,28 @@
+//CODEFORCE
+//PRAC 18 NUM
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s,t;
+  cin>>s>>t;
+  reverse(s.begin(),s.end());
+  if(s==t){
+    cout<<"YES"<<endl;
+  }
+  else{
+    cout<<"NO"<<endl;
+  }
+  return 0;
+}
+
+
+/*
+code
+edoc
+
+OUTPUT
+--
+YES
+*/
