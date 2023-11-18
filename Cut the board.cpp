cae9b878a68c3a68199a4e349372331846eabdfe
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-11-18 20:17:14 +0600

    Create Cut the board.cpp

---
+++ Cut the board.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        cout<<(n-1)*(m-1)<<endl;
+    }
+	// your code goes here
+	return 0;
+}
+/*
+4
+2 4
+3 3
+6 8
+7 5
+output-
+3
+4
+35
+24
+*/
