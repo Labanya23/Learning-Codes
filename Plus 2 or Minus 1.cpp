954b68b45d67db362c9903b6c69029027837d0b9
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-11-22 00:50:52 +0600

    Create Plus 2 or Minus 1.cpp

---
+++ Plus 2 or Minus 1.cpp
@@ -0,0 +1,22 @@
+include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n==0)
+	    {
+	        cout<<"1"<<endl;
+	    }
+	    else
+	    {
+	        cout<<3*n<<endl;
+	    }
+	}
+	return 0;
+}
